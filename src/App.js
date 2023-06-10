73d6f501e8ca8da21b0e0ead3bbc0e66e7e15b9e
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-06-10 23:37:05 +0530

    React-Basic

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import Navbar from "./Components/Navbar";
+import About from "./Components/About.js";
+import Section from "./Components/Section";
+
+
+let section_data = [
+    {title: "Skills", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim architecto ipsam voluptate soluta sunt suscipit officiis illo at autem sint eos vero provident impedit sequi, deserunt beatae, a eaque dicta ut? Quae sed totam, accusamus facere nemo exercitationem placeat facilis nesciunt ullam animi ipsa quos itaque, perspiciatis quo id quibusdam incidunt debitis qui error tempora soluta alias eius aliquid? Deserunt saepe sequi quos necessitatibus id expedita provident itaque dolore velit pariatur! Minima, ratione. Quisquam, voluptatum."},
+
+    {
+        title: "Experience",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim architecto ipsam voluptate soluta sunt suscipit officiis illo at autem sint eos vero provident impedit sequi, deserunt beatae, a eaque dicta ut? Quae sed totam, accusamus facere nemo exercitationem placeat facilis nesciunt ullam animi ipsa quos itaque, perspiciatis quo id quibusdam incidunt debitis qui error tempora soluta alias eius aliquid? Deserunt saepe sequi quos necessitatibus id expedita provident itaque dolore velit pariatur! Minima, ratione. Quisquam, voluptatum."
+    },
+
+    {
+        title: "Education",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim architecto ipsam voluptate soluta sunt suscipit officiis illo at autem sint eos vero provident impedit sequi, deserunt beatae, a eaque dicta ut? Quae sed totam, accusamus facere nemo exercitationem placeat facilis nesciunt ullam animi ipsa quos itaque, perspiciatis quo id quibusdam incidunt debitis qui error tempora soluta alias eius aliquid? Deserunt saepe sequi quos necessitatibus id expedita provident itaque dolore velit pariatur! Minima, ratione. Quisquam, voluptatum."
+    }
+]
+
+
+const App = () => {
+
+    return (
+        <div>
+            <Navbar />
+            <About />
+            
+            {
+                 section_data.map((value)=>(
+                      <Section 
+                        title={value.title}
+                        description={value.description}
+                      />
+                 ))
+
+
+            }
+            
+
+
+        </div>
+    );
+}
+
+export default App;
